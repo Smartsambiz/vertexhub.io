ca1718ba57f43fc867cf835dce2af4a582dad4a6
Samuel Dauda <smartsam9981@gmail.com>
2026-05-13 19:53:19 +0100

    added a get for the api

--- server/server.js
+++ server/server.js
@@ -20,6 +20,9 @@ mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log('MongoDB connection error:', err));
 
+app.get('/', (req, res) => {
+  res.send('Welcome to VertexHub API');
+});
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/tickets', require('./routes/tickets'));
 
