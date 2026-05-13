70820a1f2143eec15179a00a85c0940b44cdfd6c
Samuel Dauda <smartsam9981@gmail.com>
2026-05-13 19:37:28 +0100

    added versel.json

--- server/server.js
+++ server/server.js
@@ -8,7 +8,11 @@ dns.setServers(['8.8.8.8', '1.1.1.1']);
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://smartsambiz.github.io/vertexhub.io/',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+}));
 app.use(express.json());
 app.use(express.static('.'));
 
@@ -20,4 +24,6 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/tickets', require('./routes/tickets'));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+//app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = app;
